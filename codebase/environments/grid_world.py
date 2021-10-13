712cc6a5ed25f30df7c3eed2eb8f7073971ed899
Jonne Goedhart <jonne.goedhart@gmail.com>
2021-10-13 18:47:28 +0200

    update gridworld

--- codebase/environments/grid_world.py
+++ codebase/environments/grid_world.py
@@ -9,107 +9,38 @@ DOWN = 2
 LEFT = 3
 
 class GridworldEnv(stochasticEnv.stochasticEnv):
-    """
-    Grid World environment from Sutton's Reinforcement Learning book chapter 4.
-    You are an agent on an MxN grid and your goal is to reach the terminal
-    state at the top left or the bottom right corner.
+    def _limit_coordinates(self, coord):
+        coord[0] = min(coord[0], self.shape[0] - 1)
+        coord[0] = max(coord[0], 0)
+        coord[1] = min(coord[1], self.shape[1] - 1)
+        coord[1] = max(coord[1], 0)
+        return coord
 
-    For example, a 4x4 grid looks as follows:
+    def _calculate_transition_prob(self, current, delta):
+        new_position = np.array(current) + np.array(delta)
+        new_position = self._limit_coordinates(new_position).astype(int)
+        new_state = np.ravel_multi_index(tuple(new_position), self.shape)
+        is_done = tuple(new_position) == (6, 9)
+        return [(1.0, new_state, lambda: -1.0, is_done)]
 
-    T  o  o  o
-    o  x  o  o
-    o  o  o  o
-    o  o  o  T
+    def __init__(self):
+        self.shape = (7, 10)
 
-    x is your position and T are the two terminal states.
-
-    You can take actions in each direction (UP=0, RIGHT=1, DOWN=2, LEFT=3).
-    Actions going off the edge leave you in your current state.
-    You receive a reward of -1 at each step until you reach a terminal state.
-    """
-
-    metadata = {'render.modes': ['human', 'ansi']}
-
-    def __init__(self, shape=[4,4]):
-        if not isinstance(shape, (list, tuple)) or not len(shape) == 2:
-            raise ValueError('shape argument must be a list/tuple of length 2')
-
-        self.shape = shape
-
-        nS = np.prod(shape)
+        nS = np.prod(self.shape)
         nA = 4
 
-        MAX_Y = shape[0]
-        MAX_X = shape[1]
-
+        # Calculate transition probabilities
         P = {}
-        grid = np.arange(nS).reshape(shape)
-        it = np.nditer(grid, flags=['multi_index'])
+        for s in range(nS):
+            position = np.unravel_index(s, self.shape)
+            P[s] = { a : [] for a in range(nA) }
+            P[s][UP] = self._calculate_transition_prob(position, [-1, 0])
+            P[s][RIGHT] = self._calculate_transition_prob(position, [0, 1])
+            P[s][DOWN] = self._calculate_transition_prob(position, [1, 0])
+            P[s][LEFT] = self._calculate_transition_prob(position, [0, -1])
 
-        while not it.finished:
-            s = it.iterindex
-            y, x = it.multi_index
-
-            P[s] = {a : [] for a in range(nA)}
-
-            is_done = lambda s: s == 0 or s == (nS - 1)
-            reward = lambda:0.0 if is_done(s) else lambda:-1.0
-
-            # We're stuck in a terminal state
-            if is_done(s):
-                P[s][UP] = [(1.0, s, reward, True)]
-                P[s][RIGHT] = [(1.0, s, reward, True)]
-                P[s][DOWN] = [(1.0, s, reward, True)]
-                P[s][LEFT] = [(1.0, s, reward, True)]
-            # Not a terminal state
-            else:
-                ns_up = s if y == 0 else s - MAX_X
-                ns_right = s if x == (MAX_X - 1) else s + 1
-                ns_down = s if y == (MAX_Y - 1) else s + MAX_X
-                ns_left = s if x == 0 else s - 1
-                P[s][UP] = [(1.0, ns_up, reward, is_done(ns_up))]
-                P[s][RIGHT] = [(1.0, ns_right, reward, is_done(ns_right))]
-                P[s][DOWN] = [(1.0, ns_down, reward, is_done(ns_down))]
-                P[s][LEFT] = [(1.0, ns_left, reward, is_done(ns_left))]
-
-            it.iternext()
-
-        # Initial state distribution is uniform
-        isd = np.ones(nS) / nS
-
-        # We expose the model of the environment for educational purposes
-        # This should not be used in any model-free learning algorithm
-        self.P = P
+        # We always start in state (0, 0)
+        isd = np.zeros(nS)
+        isd[np.ravel_multi_index((0,0), self.shape)] = 1.0
 
         super(GridworldEnv, self).__init__(nS, nA, P, isd)
-
-    def _render(self, mode='human', close=False):
-        if close:
-            return
-
-        outfile = StringIO() if mode == 'ansi' else sys.stdout
-
-        grid = np.arange(self.nS).reshape(self.shape)
-        it = np.nditer(grid, flags=['multi_index'])
-        while not it.finished:
-            s = it.iterindex
-            y, x = it.multi_index
-
-            if self.s == s:
-                output = " x "
-            elif s == 0 or s == self.nS - 1:
-                output = " T "
-            else:
-                output = " o "
-
-            if x == 0:
-                output = output.lstrip()
-            if x == self.shape[1] - 1:
-                output = output.rstrip()
-
-            outfile.write(output)
-
-            if x == self.shape[1] - 1:
-                outfile.write("\n")
-
-            it.iternext()
