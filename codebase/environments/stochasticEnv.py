d130841298cb9dcbddbbe2a15be0b310460d3bed
Jonne Goedhart <jonne.goedhart@gmail.com>
2021-10-13 18:25:49 +0200

    stochastic env

---
+++ codebase/environments/stochasticEnv.py
@@ -0,0 +1,12 @@
+from gym.envs.toy_text import discrete
+
+rng = np.random.default_rng()
+
+class stochasticEnv(discrete.DiscreteEnv):
+    def step(self, a):
+        transitions = self.P[self.s][a]
+        i = rng.choice(len(transitions), 1, p=[t[0] for t in transitions])[0]
+        p, s, r, d = transitions[i]
+        self.s = s
+        self.lastaction = a
+        return (int(s), r(), d, {"prob": p})
